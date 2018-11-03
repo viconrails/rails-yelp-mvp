e981c49ce866374cfbc13d6aedd53c8063765c2d
victor king <victor.king@essec.edu>
2018-11-03 12:23:33 +0800

    rails new

---
+++ app/models/restaurant.rb
@@ -0,0 +1,7 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews
+  validates :name, uniqueness: true, presence: true
+  validates :address, uniqueness: true, presence: true
+  validates :phone_number, uniqueness: true, presence: true
+  validates :category, inclusion: %w(chinese italian japanese french belgian)
+end
--- db/migrate/20181103034728_create_restaurants.rb
+++ db/migrate/20181103034728_create_restaurants.rb
@@ -1,6 +1,10 @@
 class CreateRestaurants < ActiveRecord::Migration[5.2]
   def change
     create_table :restaurants do |t|
+      t.string :name
+      t.string :address
+      t.string :phone_number
+      t.string :category
 
       t.timestamps
     end
